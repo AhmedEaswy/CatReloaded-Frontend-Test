bf213b2913fbe8cd36c156dbe25dd5e0b20c1c84
AhmedEaswy <elesawy325@gmail.com>
2020-12-23 23:18:34 +0200

    test

---
+++ task-2.js
@@ -0,0 +1,10 @@
+function findAndReplace(str) {
+    let newStr = "";
+    str = str.split("");
+    str.forEach((item) => {
+      if(item === "1") {item = 0} else if (item === "0") {item = 1}
+        newStr += item;
+    })
+    return newStr;
+}
+console.log(findAndReplace("011x0x"));
